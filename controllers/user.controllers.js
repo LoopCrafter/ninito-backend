ab1ca27979ea2dd34a5d0629d7fa4452fc8f4975
Hamed <ostovari.eng@gmail.com>
2025-10-03 20:10:22 +0200

    Implemented get user profile endpoint

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -14,16 +14,46 @@ const getUserById = async (req, res) => {
   if (id !== req.userId) {
     return res.status(403).json({ message: "Access denied", success: false });
   }
-  try{
+  try {
     const user = await User.findById(id).select("-password -role");
     if (!user) {
-      return res.status(404).json({ message: "User not found", success: false });
+      return res
+        .status(404)
+        .json({ message: "User not found", success: false });
     }
     res.status(200).json({ user, success: true });
   } catch (error) {
-    res.status(500).json({ message: error.message, success: false });
+    return res.status(500).json({
+      success: false,
+      message: error.message || "خطای داخلی سرور",
+    });
   }
-  
 };
 
-export { getAllUsers, getUserById };
+const getProfile = async (req, res) => {
+  const userId = req.userId;
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      res.status(404).json({ message: "کاربر یافت نشد", success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      user: {
+        ...user._doc,
+        password: undefined,
+        role: undefined,
+        resetPasswordExpiresAt: undefined,
+        resetPasswordToken: undefined,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message || "خطای داخلی سرور",
+    });
+  }
+  res.json({ id: userId });
+};
+export { getAllUsers, getUserById, getProfile };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
-import { getAllUsers } from "../controllers/user.controllers.js";
+import { getAllUsers, getProfile } from "../controllers/user.controllers.js";
 import { authorizeRoles, requireAuth } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 router.get("/", requireAuth, authorizeRoles("admin"), getAllUsers);
+
+router.get("/profile", requireAuth, getProfile);
 export default router;
